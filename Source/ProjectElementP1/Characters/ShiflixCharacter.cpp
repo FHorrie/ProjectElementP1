11bafdf189654c71a730dbb36df004725334be2a
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-08-21 23:19:07 +0200

    update: base shiflix character

---
+++ Source/ProjectElementP1/Characters/ShiflixCharacter.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShiflixCharacter.h"
+
+#include "EnemyController.h"
+#include "ProjectElementP1/Components/EquipmentComponent.h"
+
+
+// Sets default values
+AShiflixCharacter::AShiflixCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CreateDefaultSubobject<UEquipmentComponent>("Equipment");
+}
+
+void AShiflixCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	if (AEnemyController* controllerPtr = Cast<AEnemyController>(NewController))
+	{
+		controllerPtr->OnAbilityStop.AddUObject(Equipment, &UEquipmentComponent::UseAbility);
+		controllerPtr->OnAbilityStop.AddUObject(Equipment, &UEquipmentComponent::StopAbility);
+	}
+}
---
+++ Source/ProjectElementP1/Characters/ShiflixCharacter.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShiflixCharacter.generated.h"
+
+class UEquipmentComponent;
+
+UCLASS()
+class PROJECTELEMENTP1_API AShiflixCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShiflixCharacter();
+	
+	virtual void PossessedBy(AController* NewController) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess))
+	UEquipmentComponent* Equipment{ nullptr };
+};
