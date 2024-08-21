024db4ec935106ada1c7faa3bc2457f6c6e1ece4
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-08-21 23:19:26 +0200

    fix: abilitySlot renamed

--- Source/ProjectElementP1/Behavior/Tasks/BTT_StopAbility.h
+++ Source/ProjectElementP1/Behavior/Tasks/BTT_StopAbility.h
@@ -20,5 +20,5 @@ public:
 
 private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess="true"))
-	EAbilityType AbilityType{ EAbilityType::none };
+	EAbilitySlot AbilityType{ EAbilitySlot::none };
 };
