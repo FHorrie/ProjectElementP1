f77e38d304d8f038e4ce0ed4291e35c77cbfb663
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-08-21 18:31:19 +0200

    feat: equipment allowed to use abilities
    abilities will be derived in bp

--- Source/ProjectElementP1/Components/AbilityComponent.cpp
+++ Source/ProjectElementP1/Components/AbilityComponent.cpp
@@ -2,14 +2,3 @@
 
 
 #include "AbilityComponent.h"
-
-EAbilityType UAbilityComponent::GetType() const
-{
-	return Type;
-}
-
-void UAbilityComponent::BeginPlay()
-{
-	Super::BeginPlay();
-	checkf(Type != EAbilityType::none, TEXT("No ability selected on %s"), *GetOwner()->GetName());
-}
---
+++ Source/ProjectElementP1/Components/EquipmentComponent.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EquipmentComponent.h"
+
+#include "AbilityComponent.h"
+
+UEquipmentComponent::UEquipmentComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	constexpr uint8 nrAbilities{ static_cast<uint8>(EAbilitySlot::NR_ABILITIES) };
+	AbilityTypes.SetNum(nrAbilities);
+}
+
+void UEquipmentComponent::SwitchAbility(EAbilitySlot abilitySlot, TSubclassOf<UAbilityComponent> abilityType)
+{
+	const uint8 abilityNr{ static_cast<uint8>(abilitySlot) };
+	AbilityTypes[abilityNr] = abilityType;
+	StopAbility(abilitySlot);
+	m_Abilities[abilityNr]->DestroyComponent();
+	m_Abilities[abilityNr] = CreateAbility(abilityType);
+}
+
+void UEquipmentComponent::UseAbility(EAbilitySlot abilitySlot)
+{
+	if (IsSlotValid(abilitySlot))
+		GetAbility(abilitySlot)->Use();
+}
+
+void UEquipmentComponent::StopAbility(EAbilitySlot abilitySlot)
+{
+	if (!IsSlotValid(abilitySlot))
+		return;
+
+	UAbilityComponent* abilityPtr{ GetAbility(abilitySlot) };
+	abilityPtr->Stop();
+	abilityPtr->Reset();
+}
+
+// Called when the game starts
+void UEquipmentComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Check if the dev has enough abilities
+	constexpr uint8 nrAbilities{ static_cast<uint8>(EAbilitySlot::NR_ABILITIES) };
+	if (AbilityTypes.Num() < nrAbilities)
+	{
+		AbilityTypes.SetNum(nrAbilities);
+		UE_LOG(LogTemp, Warning, TEXT("Not enough abilities found in %s"), *GetOwner()->GetName());
+	}
+	
+	// Fill abilities up
+	m_Abilities.Empty();
+	for (auto abilityType : AbilityTypes)
+		m_Abilities.Add(CreateAbility(abilityType));
+}
+
+UAbilityComponent* UEquipmentComponent::CreateAbility(TSubclassOf<UAbilityComponent> abilityType) const
+{
+	if (abilityType == nullptr)
+		return nullptr;
+	UAbilityComponent* abilityPtr{ NewObject<UAbilityComponent>(GetOwner(), abilityType.Get()) };
+	abilityPtr->RegisterComponent();
+	return abilityPtr;
+}
+
+bool UEquipmentComponent::IsSlotValid(EAbilitySlot abilitySlot) const
+{
+	const uint8 slotNr{ static_cast<uint8>(abilitySlot) };
+	if (abilitySlot > EAbilitySlot::NR_ABILITIES)
+		return false;
+	return m_Abilities[slotNr] != nullptr;
+}
+
+UAbilityComponent* UEquipmentComponent::GetAbility(EAbilitySlot abilitySlot) const
+{
+	return m_Abilities[static_cast<uint8>(abilitySlot)];
+}
+
+
