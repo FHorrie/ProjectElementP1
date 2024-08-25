224c569354a91dea71e4bf8d8d8241b57ffbe1df
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2024-08-26 00:23:37 +0200

    feat: implemented inventory comp

---
+++ Source/ProjectElementP1/Components/InventoryHandlerComponent.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventoryHandlerComponent.h"
+
+#include "AbilityComponent.h"
+
+
+bool FInventorySlot::operator==(const FInventorySlot& other) const
+{
+	return other.abilitySlot == abilitySlot;
+}
+
+bool FInventorySlot::operator==(TSubclassOf<UAbilityComponent> type) const
+{
+	return type == abilityType;
+}
+
+// Sets default values for this component's properties
+UInventoryHandlerComponent::UInventoryHandlerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UInventoryHandlerComponent::AddAbility(const FString& displayName, TSubclassOf<UAbilityComponent> abilityType, EAbilitySlot abilitySlot)
+{
+	check(abilityType == nullptr);
+
+	const FInventorySlot slot{ displayName, abilityType, abilitySlot, 0 };
+	if (!HasAbility(abilityType))
+	{
+		NewAbilityDelegate.Broadcast(slot);
+		AbilityDelegate.Broadcast(slot);
+		m_Slots.Add(slot);
+	}
+	else
+	{
+		FInventorySlot& foundSlot{ m_Slots[m_Slots.Find(slot)] };
+		++foundSlot.nrUpgradeTokens;
+		AbilityDelegate.Broadcast(foundSlot);
+	}
+}
+
+bool UInventoryHandlerComponent::HasAbility(TSubclassOf<UAbilityComponent> abilityType) const
+{
+	return m_Slots.Contains(abilityType);
+}
---
+++ Source/ProjectElementP1/Components/InventoryHandlerComponent.h
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InventoryHandlerComponent.generated.h"
+
+enum class EAbilitySlot : uint8;
+class UAbilityComponent;
+
+USTRUCT(BlueprintType)
+struct FInventorySlot
+{
+	GENERATED_BODY()
+
+	bool operator==(const FInventorySlot& other) const;
+	bool operator==(TSubclassOf<UAbilityComponent> type) const;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString displayName{};
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TSubclassOf<UAbilityComponent> abilityType;;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	EAbilitySlot abilitySlot{};
+	int32_t nrUpgradeTokens{};
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnNewAbilityCollect, const FInventorySlot&, slot);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAbilityCollect, const FInventorySlot&, slot);
+r
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTELEMENTP1_API UInventoryHandlerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UInventoryHandlerComponent();
+	
+	UFUNCTION(BlueprintCallable)
+	void AddAbility(const FString& displayName, TSubclassOf<UAbilityComponent> abilityType, EAbilitySlot abilitySlot);
+
+	UFUNCTION(BlueprintCallable)
+	bool HasAbility(TSubclassOf<UAbilityComponent> abilityType) const;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, BlueprintAssignable)
+	FOnNewAbilityCollect NewAbilityDelegate {};
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, BlueprintAssignable)
+	FOnAbilityCollect AbilityDelegate {};
+
+private:
+	TArray<FInventorySlot> m_Slots{};
+};
